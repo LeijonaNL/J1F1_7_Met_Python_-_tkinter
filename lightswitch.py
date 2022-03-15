c011a1a528555d9918cf1e4e3b091f0946b7b331
Lennert <99069053@mydavinci.nl>
2022-03-15 10:02:29 +0100

    LightSwitch

--- lightswitch.py
+++ lightswitch.py
@@ -1,12 +1,25 @@
 import tkinter as tk
 window = tk.Tk()
+window.geometry('300x200')
+window.resizable(False, False)
+window.title('Lightswitch')
 
-button = tk.Button(text='...', bg="white", fg="black")
+button = tk.Button(text='Click me', bg="white", fg="black")
 button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
 
+def toggle(self):
+    if button.config('text')[-1] == 'ON':
+        button.config(text = 'OFF')
+        window.config(bg = 'BLACK')
+        print("Power was turned OFF.")
+    else:
+        button.config(text = 'ON')
+        window.config(bg = 'YELLOW')
+        print("Power was turned ON")
 
+button.bind('<Button-1>', toggle)
 
 # schijf hier tussen je code
 
